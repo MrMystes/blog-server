4ccdeb83aa38e731b5fec06df4e9864e2564bce5
Mystes <Mystes07@163.com>
2018-01-09 15:51:18 +0800

    还原

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'blogServer',
+      script    : './bin/www',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '106.14.205.89',
+      ref  : 'origin/master',
+      repo : 'https://github.com/MrMystes/blog-server.git',
+      path : '/home/deploy/blog-server',
+      'post-deploy' : 'npm install && pm2 start ecosystem.config.js --env production'
+    }
+  }
+};
